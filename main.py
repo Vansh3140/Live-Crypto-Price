efe5b8b08ce69585083c3959a9fad3e0a9e3f588
Vansh3140 <95650499+Vansh3140@users.noreply.github.com>
2024-12-03 03:18:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def main():
             if os.path.exists(chart_filename):
                 os.remove(chart_filename)
                 logging.info(f"Temporary file '{chart_filename}' deleted.")
-            times.sleep(5)
+            time.sleep(5)
     except Exception as e:
         logging.exception(f"An error occurred: {e}")
 
